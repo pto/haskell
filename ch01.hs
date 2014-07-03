f7fa3072bb644cfa10c39fb5af55520fd301b662
Peter Olsen <pto@me.com>
2014-07-02 20:21:50 -0400

    cleaning up chapter 1

--- ch01.hs
+++ ch01.hs
@@ -1,28 +1,53 @@
+double :: Num a => a -> a
 double x = x + x
 
+mySum :: Num a => [a] -> a
 mySum [] = 0
 mySum (x:xs) = x + mySum xs
 
-qsort [] = []
+qsort :: Ord a => [a] -> [a]
+qsort []     = []
 qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
                where
-                 smaller = [ a | a <- xs, a <= x ]
-                 larger = [ b | b <- xs, b > x ]
+                    smaller = [ a | a <- xs, a <= x ]
+                    larger  = [ b | b <- xs, b > x ]
+
+-- Exercise 1
+--      double (double 2)
+-- =            { applying the outer double }
+--      double 2 + double 2
+-- =            { applying the first double }
+--      (2 + 2) + double 2
+-- =            { applying double }
+--      (2 + 2) + (2 + 2)
+-- =            { applying the first + }
+--      4 + (2 + 2)
+-- =            { applying the second + }
+--      4 + 4
+-- =            { applying + }
+--      8
+
+-- Exerise 2
+-- sum [x] = sum (x:[]) = x + sum [] = x + 0 = x
 
 -- Exercise 3
-myProduct [] = 1
+myProduct :: Num a => [a] -> a
+myProduct []     = 1
 myProduct (x:xs) = x * myProduct xs
 
 -- Exercise 4
-reverseSort [] = []
+reverseSort :: Ord a => [a] -> [a]
+reverseSort []     = []
 reverseSort (x:xs) = reverseSort larger ++ [x] ++ reverseSort smaller
-                                                where
-                                                        larger = [ a | a <- xs, a > x ]
-                                                        smaller = [ a | a <- xs, a <= x ]
+                     where
+                        larger  = [ a | a <- xs, a > x ]
+                        smaller = [ a | a <- xs, a <= x ]
 
 -- Exercise 5
-setSort [] = []
-setSort (x:xs) = setSort smaller ++ [x] ++ setSort larger
-                                        where
-                                                smaller = [ a | a <- xs, a < x ]
-                                                larger = [ a | a <- xs, a > x ]
+-- uniqueSort sorts distinct values in the input list
+uniqueSort :: Ord a => [a] -> [a]
+uniqueSort []     = []
+uniqueSort (x:xs) = uniqueSort smaller ++ [x] ++ uniqueSort larger
+                    where
+                        smaller = [ a | a <- xs, a < x ]
+                        larger  = [ a | a <- xs, a > x ]
