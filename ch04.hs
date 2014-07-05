52807d478908de8fcffda76f83be57925e3a1548
Peter Olsen <pto@me.com>
2014-07-05 09:27:49 -0400

    adding output to FizzBuzz

--- ch04.hs
+++ ch04.hs
@@ -1,8 +1,20 @@
-fizzBuzz :: Int -> [Char]
-fizzBuzz x | x `mod` 15 == 0 = "FizzBuzz"
-           | x `mod` 3 == 0  = "Fizz"
-                   | x `mod` 5 == 0  = "Buzz"
-                   | otherwise           = show x
+-- Fizzbuzz
+
+import Data.List (intercalate)
+
+fizzBuzzLine :: Int -> String
+fizzBuzzLine x | x `mod` 15 == 0 = "FizzBuzz"
+               | x `mod` 3 == 0  = "Fizz"
+               | x `mod` 5 == 0  = "Buzz"
+               | otherwise       = show x
+
+fizzBuzzList :: Int -> [String]
+fizzBuzzList n = map fizzBuzzLine [1..n]
+
+fizzBuzz :: IO ()
+fizzBuzz = putStrLn (intercalate "\n" (fizzBuzzList 100))
+
+-- Triangle
 
 nextValue :: [[Int]] -> Int
 nextValue xss = last (last xss) + 1
@@ -13,3 +25,5 @@ line x size = [x..x + size - 1]
 triangle :: Int -> [[Int]]
 triangle 1 = [[1]]
 triangle x = triangle (x - 1) ++ [line (nextValue (triangle (x - 1))) x]
+
+
