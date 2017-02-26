528c60a20638f3de35c5fafe5bc93675a0760447
Peter Olsen <pto@me.com>
2017-02-26 15:27:34 -0500

    stylistic changes

--- ch04.hs
+++ ch04.hs
@@ -1,89 +1,61 @@
--- Fizzbuzz
-fizzBuzz :: IO ()
-fizzBuzz = let fizzBuzzLine x | x `mod` 15 == 0 = "FizzBuzz"
-                              | x `mod` 3 == 0  = "Fizz"
-                              | x `mod` 5 == 0  = "Buzz"
-                              | otherwise       = show x
-               fizzBuzzList = map fizzBuzzLine [1..100]
-           in  putStr (unlines fizzBuzzList)
-        
--- Triangle
-nextValue :: [[Int]] -> Int
-nextValue xss = last (last xss) + 1
-
-line :: Int -> Int -> [Int]
-line n size = [n..n + size - 1]
-
-tri :: Int -> [[Int]]
-tri 1 = [[1]]
-tri n = tri (n - 1) ++ [line (nextValue (tri (n - 1))) n]
-
-triangle :: Int -> IO ()
-triangle n = let lineToString xs = unwords (map show xs)
-                 triToString = unlines (map lineToString (tri n))
-             in putStr triToString
-
 -- Exercise 1
 halve :: [a] -> ([a], [a])
-halve xs | even (length xs) = let half = length xs `div` 2
-                                in (take half xs, drop half xs)
-         | otherwise        = error "only works on even length lists"
+halve xs = splitAt (length xs `div` 2) xs
 
 -- Exercise 2
-third_a :: [a] -> a
-third_a xs = head (tail (tail xs))
+third :: [a] -> a
+third xs = head (tail (tail xs))
 
-third_b :: [a] -> a
-third_b xs = xs !! 2
+third' :: [a] -> a
+third' xs = xs !! 2
 
-third_c :: [a] -> a
-third_c (_:_:x:_) = x
+third'' :: [a] -> a
+third'' (_:_:x:_) = x
 
 -- Exercise 3
-safetail_a :: [a] -> [a]
-safetail_a xs = if null xs then [] else tail xs
+safetail :: [a] -> [a]
+safetail xs = if null xs then [] else tail xs
 
-safetail_b :: [a] -> [a]
-safetail_b xs | null xs   = []
-              | otherwise = tail xs
+safetail' :: [a] -> [a]
+safetail' xs | null xs   = []
+             | otherwise = tail xs
 
-safetail_c :: [a] -> [a]
-safetail_c (_:xs) = xs
-safetail_c _      = []
+safetail'' :: [a] -> [a]
+safetail'' (_:xs) = xs
+safetail'' _      = []
 
 -- Exercise 4
-or1 :: Bool -> Bool -> Bool
-or1 False False = False
-or1 True True   = True
-or1 True False  = True
-or1 False True  = True
+or' :: Bool -> Bool -> Bool
+or' False False = False
+or' True True   = True
+or' True False  = True
+or' False True  = True
 
-or2 :: Bool -> Bool -> Bool
-or2 False False = False
-or2 _ _         = True
+or'' :: Bool -> Bool -> Bool
+or'' False False = False
+or'' _ _         = True
 
-or3 :: Bool -> Bool -> Bool
-or3 True _ = True
-or3 False b = b
+or''' :: Bool -> Bool -> Bool
+or''' True _ = True
+or''' False b = b
 
-or4 :: Bool -> Bool -> Bool
-or4 a b | a == b    = a
-        | otherwise = True
+or'''' :: Bool -> Bool -> Bool
+or'''' a b | a == b    = a
+           | otherwise = True
 
 -- Exercise 5
-and_if_1 :: Bool -> Bool -> Bool
-and_if_1 a b = if a then if b then True
-                              else False
-                    else False
+and' :: Bool -> Bool -> Bool
+and' a b = if a then if b then True
+                          else False
+                else False
 
 -- Exercise 6
-and_if_2 :: Bool -> Bool -> Bool
-and_if_2 a b = if a then b
-                    else False
+and'' :: Bool -> Bool -> Bool
+and'' a b = if a then b
+                 else False
 
 -- Exercise 7
 mult :: Int -> Int -> Int -> Int
--- mult x y z = x*y*z
 mult = \x -> (\y -> (\z -> x*y*z))
 
 -- Exercise 8
@@ -93,6 +65,5 @@ luhnDouble d = if double > 9 then double - 9
                where double = d + d
 
 luhn :: Int -> Int -> Int -> Int -> Bool
-luhn a b c d = let calc = luhnDouble a + b + luhnDouble c + d
-                    in calc `mod` 10 == 0
-
+luhn a b c d = calc `mod` 10 == 0
+               where calc = luhnDouble a + b + luhnDouble c + d
